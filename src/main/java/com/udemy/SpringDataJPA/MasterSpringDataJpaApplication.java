fe156bddd75650af30cb67b3dc352ef737ad5ddb
himanshutl <himanshu.techlead@gmail.com>
2024-02-06 23:06:07 +0530

    Created setup for spring boot project

---
+++ src/main/java/com/udemy/SpringDataJPA/MasterSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/SpringDataJPA/MasterSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
